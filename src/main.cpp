0ada684c8d2e07cbd6194a5380a0b97a11d65121
ilia <rasskazo7.il@gmail.com>
2026-05-13 07:53:18 +0000

    fix: move exception handling to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
+#include "logger.h"
 #include "runner.h"
+#include <exception>
 
 int main(int argc, char **argv) {
-  Runner runner;
-  return runner.run(argc, argv);
+  try {
+    Runner runner;
+    return runner.run(argc, argv);
+  } catch (const std::exception &e) {
+    Logger::instance().error(e.what());
+    return 1;
+  } catch (...) {
+    Logger::instance().error("unknown error");
+    return 1;
+  }
 }
--- src/runner.cpp
+++ src/runner.cpp
@@ -10,22 +10,15 @@ int Runner::run(int argc, char **argv) const {
     return 0;
   }
 
-  try {
-    Logger::instance().info("start calculation");
+  Logger::instance().info("start calculation");
 
-    const Expression expression = parser_.parse(argc, argv);
-    checker_.check(expression);
+  const Expression expression = parser_.parse(argc, argv);
+  checker_.check(expression);
 
-    const int result = calculator_.calculate(expression);
-    printer_.printResult(expression, result);
+  const int result = calculator_.calculate(expression);
+  printer_.printResult(expression, result);
 
-    Logger::instance().info("calculation finished");
+  Logger::instance().info("calculation finished");
 
-    return 0;
-  } catch (const std::exception &e) {
-    Logger::instance().error(e.what());
-    printer_.printError("%s", e.what());
-    printer_.printHelp(argv[0]);
-    return 1;
-  }
+  return 0;
 }
